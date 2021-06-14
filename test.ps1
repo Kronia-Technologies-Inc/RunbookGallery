fabf73b91b812905082da2387b5debf94abe9427
OmarGRDev <85714946+OmarGRDev@users.noreply.github.com>
2021-06-14 17:30:54 -0500

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,19 @@
-Write-Output "Hello World"
+
+param (
+    [Parameter(Mandatory=$true)]
+    [string] $Command,
+
+    [Parameter(Mandatory=$false)]
+    [string] $Parameters
+)
+if ([string]::IsNullOrEmpty($Parameters))
+{
+    & $Command
+}
+else
+{
+    # Convert parameters from json into hash table so they can be passed to the command
+    $Params = @{}
+    Write-Error -Message $Command
+
+}
