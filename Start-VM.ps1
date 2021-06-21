3b5286560d1607d6701e73b15237826ca8f957a0
aldair-kr <85968165+aldair-kr@users.noreply.github.com>
2021-06-21 17:52:47 -0500

    Update Start-VM.ps1

--- Start-VM.ps1
+++ Start-VM.ps1
@@ -1,1 +1,22 @@
-Write-Output "Start-VM"
+Param
+(
+    [Parameter(Mandatory=$false)]
+    $VMResourceGroupName,
+
+    [Parameter(Mandatory=$false)]
+    $VMName,
+
+    [Parameter(Mandatory=$false)]
+    $TagName = $null
+) 
+
+$RunAsConnection = Get-AutomationConnection -Name AzureRunAsConnection
+
+Add-AzureRmAccount `
+    -ServicePrincipal `
+    -TenantId $RunAsConnection.TenantId `
+    -ApplicationId $RunAsConnection.ApplicationId `
+    -CertificateThumbprint $RunAsConnection.CertificateThumbprint | Write-Verbose
+
+Write-Output ("Starting VM " + $VMName + " in resource group " + $VMResourceGroupName)
+    Start-AzureRmVM -ResourceGroupName $VMResourceGroupName -Name $VMName | Write-Verbose
