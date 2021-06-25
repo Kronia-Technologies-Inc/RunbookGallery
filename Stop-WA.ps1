8677abe97aa8c1fd1ebd53336b710c85e2988b06
aldair-kr <85968165+aldair-kr@users.noreply.github.com>
2021-06-24 19:33:09 -0500

    Update Stop-WA.ps1

--- Stop-WA.ps1
+++ Stop-WA.ps1
@@ -19,4 +19,4 @@ Add-AzureRmAccount `
     -CertificateThumbprint $RunAsConnection.CertificateThumbprint | Write-Verbose
 
 Write-Output ("Starting WA " + $WAName + " in resource group " + $WAResourceGroupName)
-    Stop-AzWebApp -ResourceGroupName $WAResourceGroupName -Name $WAName
+    Stop-AzWebApp -ResourceGroupName $WAResourceGroupName -Name $WAName | Write-Verbose
