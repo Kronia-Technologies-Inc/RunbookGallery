80a5ead2d3e6699467d891bb6ad913532ab02be9
aldair-kr <85968165+aldair-kr@users.noreply.github.com>
2021-06-24 19:31:03 -0500

    Create Stop-WA.ps1

---
+++ Stop-WA.ps1
@@ -0,0 +1,22 @@
+Param
+(
+    [Parameter(Mandatory=$false)]
+    $WAResourceGroupName,
+
+    [Parameter(Mandatory=$false)]
+    $WAName,
+
+    [Parameter(Mandatory=$false)]
+    $TagName = $null
+) 
+
+$RunAsConnection = Get-AutomationConnection -Name AzureRunAsConnection
+
+Add-AzureRmAccount `
+    -ServicePrincipal `
+    -TenantId $RunAsConnection.TenantId `
+    -ApplicationId $RunAsConnection.ApplicationId `
+    -CertificateThumbprint $RunAsConnection.CertificateThumbprint | Write-Verbose
+
+Write-Output ("Starting WA " + $WAName + " in resource group " + $WAResourceGroupName)
+    Stop-AzWebApp -ResourceGroupName $WAResourceGroupName -Name $WAName
